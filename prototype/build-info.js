12c14c05384cb455a97156a32d23fdd510aebf6b
vicente mira <vicentmirabarrachina@gmail.com>
2026-07-31 20:27:39 +0200

    fix: invalidar cache del instalador local

--- prototype/build-info.js
+++ prototype/build-info.js
@@ -1,1 +1,1 @@
-window.INSERTIA_BUILD_INFO = {"version":"0.2.9","builtAt":"2026.07.31","revision":"da18e1b","releaseTag":"v0.2.9-stable.20260726"};
+window.INSERTIA_BUILD_INFO = {"version":"0.2.9","builtAt":"2026.07.31","revision":"627d98d","releaseTag":"v0.2.9-stable.20260726"};
